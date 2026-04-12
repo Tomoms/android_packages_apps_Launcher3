20d3d9e624c6ff45a2658750ffaa665e3e18edb3
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-13 06:15:30 +0800

    Launcher3: Stopping taskbar draws when stashed
    taskbar window was producing 601 frames (60fps) during all-apps fling even when fully stashed with alpha=0. ViewRootImpl still scheduled draws for the transparent window competing for surfaceflinger buffers causing 54% buffer stuffing. setting taskbar view INVISIBLE when stashed stops draw scheduling, VISIBLE on unstash
    
    Change-Id: I5b2f7c7d53b0fd8c3a5192db3b2f8e8a7079e79e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarStashController.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarStashController.java
@@ -1219,6 +1219,7 @@ public class TaskbarStashController implements TaskbarControllers.LoggableTaskba
             mControllers.stashedHandleViewController.onIsStashedChanged(
                     isStashed && supportsVisualStashing());
             mControllers.taskbarInsetsController.onTaskbarOrBubblebarWindowHeightOrInsetsChanged();
+            mControllers.taskbarViewController.setTaskbarViewVisible(!isStashed);
         });
         updateIsTaskbarStashed(isStashed);
     }
